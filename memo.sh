f3df73dba677a28cdfaed94f68656b208e1e7088
David Yon <david@mobile>
2019-11-23 10:05:05 +0800

    update

---
+++ memo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat $1 | grep -e "- MF Detailed" -A 13 > MF.memo
+cat $1 | grep -e "- SMF Detailed" -A 13 > SMF.memo
+cat $1 | grep -e "- Memory by Top" -A 3 > top.memo 
+cat MF.memo | grep -e "- MF Detailed" | cut -d ' ' -f 5,6,7 --output-delimiter='_' > timer.memo
+cat MF.memo | grep -e "VmData" | cut -d ' ' -f 2 > MF.VmData.memo
+cat SMF.memo | grep -e "VmData" | cut -d ' ' -f 2 > SMF.VmData.memo
+cat top.memo | grep -e " MF.bin" | cut -d ' ' -f 11 > MF.percpu.memo
+cat top.memo | grep -e " MF.bin" | cut -d ' ' -f 12 > MF.permemo.memo
+cat top.memo | grep -e " SMF.bin" | cut -d ' ' -f 11 > SMF.percpu.memo
+cat top.memo | grep -e " SMF.bin" | cut -d ' ' -f 12 > SMF.permemo.memo
