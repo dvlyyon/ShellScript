2ea2c9fe6b8a3913d1c342b188da979d0f4e0774
David Yon <david@mobile>
2019-11-26 23:08:40 +0800

    update

--- memo.sh
+++ memo.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cat $1 | grep -e "- MF Detailed" -A 13 > MF.memo
-cat $1 | grep -e "- SMF Detailed" -A 13 > SMF.memo
-cat $1 | grep -e "- Memory by Top" -A 3 > top.memo 
+heapconvert.py $1
+cat $1.cvt | grep -e "- MF Detailed" -A 18 > MF.memo
+cat $1.cvt | grep -e "- SMF Detailed" -A 15 > SMF.memo
+cat $1.cvt | grep -e "- Memory by Top" -A 3 > top.memo 
+
 cat MF.memo | grep -e "- MF Detailed" | cut -d ' ' -f 5,6,7,9 > timer.memo
 cat MF.memo | grep -e "VmData" | cut -d ' ' -f 2 > MF.VmData.memo
+cat MF.memo | grep -e "VmRSS" | cut -d ' ' -f 2 > MF.VmRss.memo
+cat MF.memo | grep -e "Threads:" | cut -d ' ' -f 2 > MF.Thread.memo
+cat MF.memo | grep -e "heap:" | cut -d ' ' -f 2 > MF.heap.memo
 cat SMF.memo | grep -e "VmData" | cut -d ' ' -f 2 > SMF.VmData.memo
 cat top.memo | grep -e " MF.bin" | cut -d ' ' -f 11 > MF.percpu.memo
 cat top.memo | grep -e " MF.bin" | cut -d ' ' -f 12 > MF.permemo.memo
